ba4312a350e5e4614c6aa72cbb4a35ff3cac6969
CedricNoyel <cedric.noyel@gmail.com>
2020-11-06 09:58:35 +0100

    add feature: ONE

--- src/main/Feature.java
+++ src/main/Feature.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Feature {
 
+	// WIDTH
 	public static List<Integer> width(List<List<Integer>> resultsPattern) {
 		List<Integer> results = new ArrayList<>();
 		
@@ -17,7 +18,7 @@ public class Feature {
 		return results;
 	}
 	
-
+	// MIN
 	public static List<Integer> min(List<List<Integer>> resultsPattern) {
 		List<Integer> results = new ArrayList<>();
 		
@@ -34,7 +35,7 @@ public class Feature {
 		return results;
 	}
 	
-
+	// MAX
 	public static List<Integer> max(List<List<Integer>> resultsPattern) {
 		List<Integer> results = new ArrayList<>();
 		
@@ -50,5 +51,33 @@ public class Feature {
 		}
 		return results;
 	}
+	
+	// ONE
+	public static List<Integer> one(List<List<Integer>> resultsPattern) {
+		List<Integer> results = new ArrayList<>();
+		
+		for (List<Integer> serieInPattern : resultsPattern) {
+			results.add(serieInPattern.get(0));
+		}
+		return results;
+	}
+
+	// RANGE
+	// HEIGHT
+
+	// SURF - Attention surf doit prendre en compte une donnée du offset END
+//	public static List<Integer> surf(List<List<Integer>> resultsPattern) {
+//		List<Integer> surfResult = new ArrayList<>();
+//		
+//		Integer surf;
+//		for (List<Integer> serieInPattern : resultsPattern) {
+//			surf = 0;
+//			for (int value : serieInPattern) {
+//				surf = surf + value;
+//			}
+//			surfResult.add(surf);
+//		}
+//		return surfResult;
+//	}
 	
 }
