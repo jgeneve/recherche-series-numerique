fd464ac503f91372be5089c82a2a852e4f1b7e7c
CedricNoyel <cedric.noyel@gmail.com>
2020-11-09 14:28:51 +0100

    add signature test 2

--- src/tests/TestAggregators.java
+++ src/tests/TestAggregators.java
@@ -9,7 +9,7 @@ import main.Aggregator;
 
 public class TestAggregators extends TestCase {
 	
-	
+	// TEST AGGREGATORS
 	public void test_MIN_Aggregator() throws Exception {
 		List<Integer> features = Arrays.asList(3,4,6,3);
 		Integer expected = 3;
@@ -21,5 +21,14 @@ public class TestAggregators extends TestCase {
 		Integer expected = 6;
 		assertEquals(expected, Aggregator.max(features));
 	}
+	
+	// TEST AGGREGATORS ERRORS
+//	public void test_MIN_Aggregator_ERROR() throws Exception {
+//		List<Integer> features = Arrays.asList();
+//		Integer expected = ;
+//		assertEquals(expected, Aggregator.min(features));
+//	}
+
+	
 	
 }
--- src/tests/TestSignature.java
+++ src/tests/TestSignature.java
@@ -7,10 +7,15 @@ import junit.framework.TestCase;
 import main.Patterns;
 
 public class TestSignature extends TestCase {
-	
-	public void testSignature() throws Exception {
+
+	public void testSignature1() throws Exception {
 		assertEquals("=>=<<=<>>=<=====>", Patterns.getSignature(new ArrayList<>(
 	            Arrays.asList(4, 4, 2, 2, 3, 5, 5, 6, 3, 1, 1, 2, 2, 2, 2, 2, 2, 1))));
+	}
+	
+	public void testSignature2() throws Exception {
+		assertEquals("<<=>=><><><>=<=", Patterns.getSignature(new ArrayList<>(
+	            Arrays.asList(1,2,6,6,4,4,3,5,2,5,1,5,3,3,4,4))));
 	}
 	
 
